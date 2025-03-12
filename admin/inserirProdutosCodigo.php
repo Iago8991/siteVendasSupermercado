bbce571704953dba8e3fdf8596be0ed39aac60ed
iago8991 <iagoadsprogramer@gmail.com>
2025-03-12 16:02:12 -0300

    Alteração dos nomes de variaveis e criação dos arquivos de css

--- admin/inserirProdutosCodigo.php
+++ admin/inserirProdutosCodigo.php
@@ -1,7 +1,6 @@
 <?PHP
-    session_start();
     require('../bd_config.php');
-    error_reporting(0);
+    //error_reporting(0);
     
     if ($_SERVER['REQUEST_METHOD'] === 'POST'){
         //Recebe os dados do formulário 
@@ -13,41 +12,41 @@
         //Processa o Upload da imagem
         if (isset($_FILES['imagem']) && $_FILES['imagem']['error'] === UPLOAD_ERR_OK){
             $imagemtmp = $_FILES['imagem']['tmp_name'];
-            $imagemnome = basename($_FILES['imagem']['name']);
-            $caminhoimagem = "../upload_produtos/" . $imagemnome; 
+            $imagemNome = basename($_FILES['imagem']['name']);
+            $caminhoImagem = "../upload_produtos/" . $imagemNome; 
              //__DIR__ retorna o caminho absoluto do diretório onde o php está localizado.
 
             //Move a imagem para a pasta de uploads
-            if(move_uploaded_file($imagemtmp, $caminhoimagem)){
+            if(move_uploaded_file($imagemtmp, $caminhoImagem)){
                 //Insere os dados no banco
                 $sql = "INSERT INTO produtos (produtos_nome, produtos_descricao, produtos_preco, produtos_imagem, produtos_estoque, produtos_desconto)
                 VALUES (?, ?, ?, ?, ?, ?)";
                 $stmt = mysqli_prepare($con, $sql);
-                mysqli_stmt_bind_param($stmt, "ssdsii", $nome, $descricao, $preco, $caminhoimagem, $estoque, $desconto);
+                mysqli_stmt_bind_param($stmt, "ssdsii", $nome, $descricao, $preco, $caminhoImagem, $estoque, $desconto);
                 
                 if (mysqli_stmt_execute($stmt)){
                     $_SESSION['sucesso'] = 'Produto Inserido com sucesso!.';
-                    header("Location: inserir_produtos.php");
+                    header("Location: inserirProdutos.php");
                 } else {
                     $_SESSION['erro'] = 'Erro ao inserir o produto:' . mysqli_error($con);
-                    header("Location: inserir_produtos.php");
+                    header("Location: inserirProdutos.php");
                     exit;
                 }
                 
                 mysqli_stmt_close($stmt);
             } else {
                 $_SESSION['erro'] = 'Erro ao fazer upload da imagem.';
-                header("Location: inserir_produtos.php");
+                header("Location: inserirProdutos.php");
                 exit;
             }
         } else {
             $_SESSION['erro'] = 'Nenhuma imagem enviada devido a erro no upload. (Verifique e tente novamente)';
-            header("Location: inserir_produtos.php");
+            header("Location: inserirProdutos.php");
             exit;
         }
     } else {
         $_SESSION['erro'] = 'Erro formulario não recebido.';
-            header("Location: inserir_produtos.php");
+            header("Location: inserirProdutos.php");
         exit;
     }
 
--- login_registro/login.php
+++ login_registro/login.php
@@ -1,42 +1,18 @@
 <?php
-session_start();
+    require('./css/login.css');
 ?>
 
-<style>
-			#login{
-				background: gray;
-				border-radius: 10px;
-				width: 40%;
-				height: 100%;
-				margin: 0px auto;
-				padding: 5px;
-			}
-            .mensagem {
-                color:red;
-                text-align: center;
-                margin-bottom: 10px;
-            }
-            .mensagem.sucesso {
-                color: green;
-            }
-		</style>
-
 <div id="login">
-        
-    <!--mensagens de erro -->
     <?php
     if(isset($_SESSION['erro'])) {
         echo "<div class = 'mensagem'> {$_SESSION['erro']} </div>";
         unset($_SESSION['erro']);
     } elseif (isset($_SESSION['sucesso'])) {
-        echo "<div class = 'mensagem sucesso'> {$_SESSION['sucesso']} </div>";
+        echo "<div class = 'mensagemSucesso'> {$_SESSION['sucesso']} </div>";
         unset($_SESSION['sucesso']);
     }
 
     ?>
-
-
-
     <form action="login_codigo.php" method="POST"> 
         <center>
             <label for="email">E-mail:</label>
@@ -47,7 +23,6 @@ session_start();
             <label for="senha">Senha:</label>
             <input type="text" min="0" max="200" name="senha" id="senha" required> 
         </center>
-
         <input type="submit" value="Enter">
     </form>
 </div>
