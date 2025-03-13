b807f5e44ab0e661b534eb5f38713a8add2d96c0
iago8991 <iagoadsprogramer@gmail.com>
2025-03-13 16:49:30 -0300

    Alteração dos nomes de variaveis e criação dos arquivos de css #2 e criação do arquivo de logoff

--- login_registro/login.php
+++ login_registro/login.php
@@ -1,19 +1,18 @@
 <?php
-    require('./css/login.css');
+    require('../css/login.css');
 ?>
 
 <div id="login">
     <?php
-    if(isset($_SESSION['erro'])) {
-        echo "<div class = 'mensagem'> {$_SESSION['erro']} </div>";
-        unset($_SESSION['erro']);
-    } elseif (isset($_SESSION['sucesso'])) {
-        echo "<div class = 'mensagemSucesso'> {$_SESSION['sucesso']} </div>";
-        unset($_SESSION['sucesso']);
-    }
-
+        if(isset($_SESSION['erro'])) {
+            echo "<div class = 'mensagem'> {$_SESSION['erro']} </div>";
+            unset($_SESSION['erro']);
+        } elseif (isset($_SESSION['sucesso'])) {
+            echo "<div class = 'mensagemSucesso'> {$_SESSION['sucesso']} </div>";
+            unset($_SESSION['sucesso']);
+        }
     ?>
-    <form action="login_codigo.php" method="POST"> 
+    <form action="loginCodigo.php" method="POST"> 
         <center>
             <label for="email">E-mail:</label>
             <input type="text" min="0" max="200" name="email" id="email" required> 
