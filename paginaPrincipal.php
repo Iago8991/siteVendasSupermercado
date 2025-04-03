42bc42a6b9a3e82ef2715afbf9427e72cfdbf257
iago8991 <iagoadsprogramer@gmail.com>
2025-04-03 18:05:29 -0300

    Alterando css do menu lateral

--- paginaPrincipal.php
+++ paginaPrincipal.php
@@ -11,44 +11,44 @@ if (isset($_SESSION['login']) && $_SESSION['login'] == 'logado'){
     <meta charset="UTF-8">
     <title>Página Principal</title>
     <link rel="stylesheet" href="css/paginaPrincipal.css">
-    <style>
-        
-    </style>
 </head>
 <body>
     <h1>Bem-vindo à Loja</h1>
-    
+
     <!-- Menu lateral fixo -->
     <div class="menu-lateral" id="menuLateral">
-    <a class="menu-item" onclick="location.href='paginaPrincipal.php';">
-            <img src="imagens/home.webp" alt="Página Principal">
-            <span>Página Principal</span>
-        </a>
-        <a class="menu-item" onclick="location.href='carrinho.php';">
-            <img src="imagens/carrinho.jpg" alt="Carrinho">
-            <span>Carrinho</span>
-        </a>
-        <a class="menu-item" onclick="location.href='exibirProdutos.php';">
-            <img src="imagens/loja.webp" alt="Loja">
-            <span>Loja</span>
-        </a>
-
-        <?php if (isset($_SESSION['admin']) && $_SESSION['admin'] == 'logado'){ ?>
-        <a class="menu-item" onclick="location.href='admin/inserirProdutos.php';">
-            <img src="imagens/inserir.jpg" alt="Inserir Produtos">
-            <span>Inserir Produtos</span>
-        </a>
-        <a class="menu-item" onclick="location.href='admin/gerenciamentoProdutos.php';">
-            <img src="imagens/gerenciamento.jpg" alt="Gerenciamento Produtos">
-            <span>Gerenciamneto Produtos</span>
-        </a>
-        <?php } else { echo ""; } ?>
-
-        <a class="menu-item" onclick="location.href='loginRegistro/logout.php';">
-            <img src="imagens/Sair.jpg" alt="Sair">
-            <span>Sair</span>
-        </a>
+        <div class="menu-items">
+            <a class="menu-item" onclick="location.href='paginaPrincipal.php';">
+                <img src="imagens/home.webp" alt="Página Principal">
+                <span>Página Principal</span>
+            </a>
+            <a class="menu-item" onclick="location.href='carrinho.php';">
+                <img src="imagens/carrinho.png" alt="Carrinho">
+                <span>Carrinho</span>
+            </a>
+            <a class="menu-item" onclick="location.href='exibirProdutos.php';">
+                <img src="imagens/loja.webp" alt="Loja">
+                <span>Loja</span>
+            </a>
+            <?php if (isset($_SESSION['admin']) && $_SESSION['admin'] == 'logado'){ ?>
+                <a class="menu-item" onclick="location.href='admin/inserirProdutos.php';">
+                    <img src="imagens/inserir.jpg" alt="Inserir Produtos">
+                    <span>Inserir Produtos</span>
+                </a>
+                <a class="menu-item" onclick="location.href='admin/gerenciamentoProdutos.php';">
+                    <img src="imagens/gerenciarProdutos.png" alt="Gerenciamento Produtos">
+                    <span>Gerenciamento Produtos</span>
+                </a>
+            <?php } ?>
+        </div>
+        <div class="menu-footer">
+            <a class="menu-item" onclick="location.href='loginRegistro/logout.php';">
+                <img src="imagens/sair.png" alt="Sair">
+                <span>Sair</span>
+            </a>
+        </div>
     </div>
+
     
     <!-- Conteúdo principal que será empurrado -->
     <div class="main-content" id="mainContent">
@@ -75,14 +75,14 @@ if (isset($_SESSION['login']) && $_SESSION['login'] == 'logado'){
         // empurra o conteudo para o lado quand o menu lateral e expandido
         const menu = document.getElementById('menuLateral');
         const mainContent = document.getElementById('mainContent');
-        
+
         menu.addEventListener('mouseenter', () => {
             mainContent.style.marginLeft = '180px';
         });
         menu.addEventListener('mouseleave', () => {
             mainContent.style.marginLeft = '50px';
         });
-        
+
         function adicionaraocarrinho(produtos_id) {
             alert("Produto " + produtos_id + " adicionado ao carrinho (exemplo)!");
         }
