4a0c3f5f84e7aef9ba1fdb717246ab919619b102
iago8991 <iagoadsprogramer@gmail.com>
2025-04-21 17:41:26 -0300

    Arrumando erros e tentativa de deixar o site online

--- bd_config.php
+++ bd_config.php
@@ -4,6 +4,17 @@
     $password = "";
     $dbname = "bd_supermercado";
 
+    // conexão no site online
+    if(isset($_SERVER['HTTP_HOST']) &&
+      ($_SERVER['HTTP_HOST'] === 'iagoRighetti.infinityfreeapp.com' ||
+      $_SERVER['HTTP_HOST'] === 'www.iagoRighetti.infinityfreeapp.com')
+      ){
+        $host = '';
+        $user = '';
+        $password = '';
+        $dbname = '';
+      }
+
     $con = new mysqli($host, $user, $password, $dbname);
     if ($con->connect_error) {
         die("Conexão falhou: " . $con->connect_error);
