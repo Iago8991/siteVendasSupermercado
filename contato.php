e2557dd91aad2f1518893991fb9703ed67adafa0
iago8991 <iagoadsprogramer@gmail.com>
2025-05-02 23:37:52 -0300

    Arrumando os arquivos para o site online #4

--- contato.php
+++ contato.php
@@ -5,6 +5,9 @@
         <p class="email">iagoadsprogramer@gmail.com</p>
         <p>Ou pelo telefone:</p>
         <p class="telefone">(11) 91373-4356</p>
+        <br/>
+        <p>Se preferir, preencha o formulário abaixo:</p>
+        <textarea class="mensagem" rows="4" cols="50" placeholder="Digite seu problema"> </textarea>
 
         <a href="paginaPrincipal.php" class="btn btn-primary">Voltar</a>
 
