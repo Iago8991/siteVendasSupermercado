24d07a5329567c1f81e31d37dbccb436bfa904b1
iago8991 <iagoadsprogramer@gmail.com>
2025-05-09 22:41:40 -0300

    Concertando o slider da pagina Principal

--- paginaPrincipal.php
+++ paginaPrincipal.php
@@ -23,45 +23,49 @@
     
         <div class="secaoEmDestaque">
             <h2 class="tituloSecao">Em Destaque</h2>
-            <div class="sliderProdutos">
-                <?php while ($produto = mysqli_fetch_array($resultadoEmDestaque)) { ?>
-                    <div class="produtoCard">
-                        <h3 class="produtoNome"> <?= htmlspecialchars($produto['produtos_nome']) ?> </h3>
-                        <img class="produtoImagem" src="./uploadProdutos/<?= htmlspecialchars($produto['produtos_imagem']) ?>" alt="<?= htmlspecialchars($produto['produtos_nome']) ?>">
-                        <p class="produtoPreco">R$ <?= number_format($produto['produtos_preco'], 2, ',', '.') ?> </p>
-                        <?php if (isset($produto['produtos_desconto']) && $produto['produtos_desconto'] > 0) { ?>
-                            <p class="produtoDesconto" style="color: red;"> Desconto: <?= $produto['produtos_desconto'] ?> % </p>
-                        <?php } ?>
-                        <div class="produtoInfoExtra">
-                            <p> <?= htmlspecialchars($produto['produtos_descricao']) ?> </p>
-                            <button onclick="adicionarAoCarrinho(<?= $produto['produtos_id'] ?>)"> Comprar </button>
+            <div class="sliderContainer">
+                <div class="sliderProdutos">
+                    <?php while ($produto = mysqli_fetch_array($resultadoEmDestaque)) { ?>
+                        <div class="produtoCard">
+                            <h3 class="produtoNome"> <?= htmlspecialchars($produto['produtos_nome']) ?> </h3>
+                            <img class="produtoImagem" src="./uploadProdutos/<?= htmlspecialchars($produto['produtos_imagem']) ?>" alt="<?= htmlspecialchars($produto['produtos_nome']) ?>">
+                            <p class="produtoPreco">R$ <?= number_format($produto['produtos_preco'], 2, ',', '.') ?> </p>
+                            <?php if (isset($produto['produtos_desconto']) && $produto['produtos_desconto'] > 0) { ?>
+                                <p class="produtoDesconto" style="color: red;"> Desconto: <?= $produto['produtos_desconto'] ?> % </p>
+                            <?php } ?>
+                            <div class="produtoInfoExtra">
+                                <p> <?= htmlspecialchars($produto['produtos_descricao']) ?> </p>
+                                <button onclick="adicionarAoCarrinho(<?= $produto['produtos_id'] ?>)"> Comprar </button>
+                            </div>
                         </div>
-                    </div>
-                <?php } ?>
+                    <?php } ?>
+                </div>
+                <div class="navegacaoSlider">
+                    <span class="setaAnterior"> &#8249; </span>
+                    <span class="setaProxima"> &#8250; </span>
+                </div> 
             </div>
-            <div class="navegacaoSlider">
-                <span class="setaAnterior"> &#8249; </span>
-                <span class="setaProxima"> &#8250; </span>
-            </div> 
         </div>
         <div class="secaoProdutos">
             <h2 class="tituloSecao"> Produtos </h2>
-            <div class="sliderProdutos">
-                <?php while ($produto = mysqli_fetch_array($resultadoProdutos)) { ?>
-                    <div class="produtoCard">
-                        <h3 class="produtoNome"> <?= htmlspecialchars($produto['produtos_nome']) ?> </h3>
-                        <img class="produtoImagem" src="./uploadProdutos/<?= htmlspecialchars($produto['produtos_imagem']) ?>" alt="<?= htmlspecialchars($produto['produtos_nome']) ?>">
-                        <p class="produtoPreco"> R$ <?= number_format($produto['produtos_preco'], 2, ',', '.') ?> </p>
-                        <div class="produtoInfoExtra">
-                            <p> <?= htmlspecialchars($produto['produtos_descricao']) ?> </p>
-                            <button onclick="adicionarAoCarrinho(<?= $produto['produtos_id'] ?>)"> Comprar </button>
+            <div class="sliderContainer">        
+                <div class="sliderProdutos">
+                    <?php while ($produto = mysqli_fetch_array($resultadoProdutos)) { ?>
+                        <div class="produtoCard">
+                            <h3 class="produtoNome"> <?= htmlspecialchars($produto['produtos_nome']) ?> </h3>
+                            <img class="produtoImagem" src="./uploadProdutos/<?= htmlspecialchars($produto['produtos_imagem']) ?>" alt="<?= htmlspecialchars($produto['produtos_nome']) ?>">
+                            <p class="produtoPreco"> R$ <?= number_format($produto['produtos_preco'], 2, ',', '.') ?> </p>
+                            <div class="produtoInfoExtra">
+                                <p> <?= htmlspecialchars($produto['produtos_descricao']) ?> </p>
+                                <button onclick="adicionarAoCarrinho(<?= $produto['produtos_id'] ?>)"> Comprar </button>
+                            </div>
                         </div>
-                    </div>
-                <?php } ?>
-            </div>
-            <div class="navegacaoSlider">
-                <span class="setaAnterior"> &#8249; </span>
-                <span class="setaProxima"> &#8250; </span>
+                    <?php } ?>
+                </div>
+                <div class="navegacaoSlider">
+                    <span class="setaAnterior"> &#8249; </span>
+                    <span class="setaProxima"> &#8250; </span>
+                </div>
             </div>
         </div>                 
 
@@ -70,26 +74,24 @@
                     alert("Produto " + produtos_id + " adicionado ao carrinho (exemplo)!");
                 }
             document.addEventListener('DOMContentLoaded', function() {
-                document.querySelectorAll('.sliderProdutos').forEach(slider => {
-                    const navegacao = slider.nextElementSibling;
-                    const setaAnterior = navegacao.querySelector('.setaAnterior');
-                    const setaProxima = navegacao.querySelector('.setaProxima');
+                document.querySelectorAll('.sliderContainer').forEach(container => {
+                    const slider = container.querySelector('.sliderProdutos');
+                    const setaPrev = container.querySelector('.setaAnterior');
+                    const setaNext = container.querySelector('.setaProxima');
 
                     if (slider.scrollWidth > slider.clientWidth) {
-                        slider.classList.add('scrollable');
-                        navegacao.classList.add('scrollable');
-                    }
+                    slider.classList.add('scrollable');
+                    container.querySelector('.navegacaoSlider').classList.add('scrollable');
 
-                    if (slider.classList.contains('scrollable')) {
-                        setaAnterior.addEventListener('click', () => {
-                            slider.scrollBy({ left: -200, behavior: 'smooth' });
-                        });
-                        setaProxima.addEventListener('click', () => {
-                            slider.ScrollBy({ left: 200, behavior: 'smooth' });
-                        });
+                    setaPrev.addEventListener('click', () => {
+                        slider.scrollBy({ left: -slider.clientWidth * 0.8, behavior: 'smooth' });
+                    });
+                    setaNext.addEventListener('click', () => {
+                        slider.scrollBy({ left: slider.clientWidth * 0.8, behavior: 'smooth' });
+                    });
                     }
                 });
-            });  
+                });  
         </script>
     </body>
 </html>
