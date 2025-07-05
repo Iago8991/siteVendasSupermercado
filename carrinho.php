a7f47be8db8e434624dd0a14884776035e6c2d8f
iago8991 <iagoadsprogramer@gmail.com>
2025-07-05 14:05:29 -0300

    Iniciando carrinho

--- carrinho.php
+++ carrinho.php
@@ -9,19 +9,24 @@
 
 <?php menuLateral::render(); ?>
 
-    <div id="mainContent">
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Carrinho</title>
+        <link rel="stylesheet" href="<?= BASE_URL ?>css/carrinho.css">
+    </head>
+    <body>
+            <div id="mainContent">
+                <h1> Carrinho de compras</h1>
 
-
-
-    
-    </div>
+            </div>
+    </body>
+</html>
 <?php
     } else {
         echo "Realize login para acessar a página!";
         echo "<button class='btn-voltar' onclick=\"location.href='index.php'\">Voltar</button>";
     }
 ?>
-<!DOCTYPE html>
-<html lang="pt-BR">
-
-</html>
