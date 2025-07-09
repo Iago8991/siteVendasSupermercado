8e6c651261037a7573547dc6db7d69f52944d9b7
iago8991 <iagoadsprogramer@gmail.com>
2025-07-09 18:40:31 -0300

    Separando arquivos javaScript

--- paginaPrincipal.php
+++ paginaPrincipal.php
@@ -87,30 +87,6 @@ if (isset($_SESSION['login']) && $_SESSION['login'] == 'logado') {
                 </div>
             </div>
         </div>
-
-        <script>
-        function adicionarAoCarrinho(produtos_id) {
-            alert("Produto " + produtos_id + " adicionado ao carrinho (exemplo)!");
-        }
-
-        document.addEventListener('DOMContentLoaded', function() {
-            document.querySelectorAll('.sliderContainer').forEach(container => {
-                const slider = container.querySelector('.sliderProdutos');
-                const prev = container.querySelector('.setaAnterior');
-                const next = container.querySelector('.setaProxima');
-
-                if (slider.scrollWidth > slider.clientWidth) {
-                    container.querySelector('.navegacaoSlider').classList.add('scrollable');
-                    prev.addEventListener('click', () => {
-                        slider.scrollBy({ left: -slider.clientWidth * 0.8, behavior: 'smooth' });
-                    });
-                    next.addEventListener('click', () => {
-                        slider.scrollBy({ left: slider.clientWidth * 0.8, behavior: 'smooth' });
-                    });
-                }
-            });
-        });
-        </script>
     </div>
 </body>
 </html>
